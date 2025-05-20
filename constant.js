369759d16e94e9e2f7a7a758a23482fd567b3356
Sequen5 <52377112+Sequen5@users.noreply.github.com>
2025-05-21 01:26:17 +0700

    Add files via upload

---
+++ constant.js
@@ -0,0 +1,86 @@
+export const summaryData = [
+  {
+    innterText: `<div class="typewriter"><p>Pengalaman</p></div>`,
+    paragraphLines: `Sering bermain game salah satunya game moba, sering ikut tournament pastinya dan selalu kalah saat semi final :v, 
+     Biasanya gabut sering main minecraft buat isi waktu luang dan main barang bersama teman pastinya, 
+     Ketika gabut saya sering membuat project kecil-kecilan seperti game, website, dan lain-lain.
+      Yang pastinya tidak akan bosan untuk belajar dan terus mengikuti perkembangan.`,
+    tiitleWord: "Pengalaman",
+    subtitleWord:"3 tahun pengalaman dalam pengembangan perangkat lunak"
+ 
+  },
+  {
+    innterText: `<div class="typewriter"><p>Projects</p></div>`,
+    paragraphLines: `Programming Languages: HTML, CSS, JavaScript, Node.js (if applicable for scripting)
+                Version Control & Collaboration Tools: Git, GitLab, GitHub
+                Create Minecraft Server:  Setup, LuckyPerms, Template Server and others.`,
+    subtitleWord: "Lagi tahap belajar",
+    tiitleWord: "Projects",
+  },
+  {
+    innterText: `<div class="typewriter"><p>Skills</p></div>`,
+    paragraphLines: `Lulusan Teknik Lingkungan yang paham tentang Air Pastinya :v, 
+    Bisa membuat Website, Discord bot dan minecraft server. 
+    Jago bermain Honor Of Kings dan Mobile Legends, Mabar ? Gaskenn
+     Asal jangan BEBAN AJA :v.`,
+    subtitleWord: "",
+    tiitleWord: "Skills",
+  },
+  {
+    innterText: `<div class="typewriter"><p>Working On</p></div>`,
+    paragraphLines: `Lagi mencari pekerjaan, siapa tau ada yang mau infokan loker :v.`,
+    subtitleWord: "",
+    tiitleWord: "Working On",
+  },
+  {
+    innterText: `<div class="typewriter"><p>About me</p></div>`,
+    paragraphLines: `"Hi, Saya Sequen seseorang yang penasaran, ingin tahu dan terbuka untuk berdiskusi.
+     Saya mempunyai hobi dan minat yang membuat hariku lebih berwarna :v.
+      Aku sering bermain badminton dan juga bermain game, Selain itu, aku juga senang berbincang dan terbuka untuk diskusi yang menarik
+       Buatku, hidup adalah perjalanan untuk terus belajar dan bertumbuh."`,
+    subtitleWord: "",
+    tiitleWord: "About me",
+  },
+  {
+    innterText: `<div class="typewriter"><p>Minecarft</p></div>`,
+    paragraphLines: `Saya mempunyai Server Minecraft Survival, Creative, dan PVP.
+     Siapapun boleh masuk java ataupun badrock, 
+     Minecraft Server :
+      Addres :sequen.fun, 
+      Port   :2507,
+       Enjoy dan Jangan Rusuk aja. Jika ada Error Bisa Hubungi Saya `,
+    subtitleWord: "Minecraft",
+    tiitleWord: "Minecraft",
+  }
+];
+export const skillsData = {
+  Html: ` <h3 class="">Html</h3>
+            
+            <p class="normalFont text-justify">Dalam Tahap Belajar.
+              Perspiclaboriosam nulla ea ab
+              Doa in semoga menguasi hehe.</p>
+              <p class="normalFont text-justify">Dalam Tahap Belajar.
+               
+                Doa in semoga menguasi hehe.</p>`,
+  Css: ` <h3 class="">Css</h3>
+            
+            <p class="normalFont text-justify">Dalam Tahap Belajar.
+              
+              Doa in semoga menguasi hehe.</p>
+              <p class="normalFont text-justify">
+                Doa in semoga menguasi hehe.</p>`,
+  NodeJS: ` <h3 class="">Nodejs</h3>
+            
+            <p class="normalFont text-justify">Dalam Tahap Belajar.
+              
+              Doa in semoga menguasi hehe.</p>
+              `,
+  Git: ` <h3 class="">Git</h3>
+            
+            <p class="normalFont text-justify">Dalam Tahap Belajar.
+              
+              Doa in semoga menguasi hehe.</p>
+              <p class="normalFont text-justify">Dalam Tahap Belajar.
+                
+                repellatt ratione voluptas.</p>`,
+}
